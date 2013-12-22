02a1196f00d2e321a690d704098df0b24f86d706
Binky Bear <binkybear@local>
2013-12-22 13:48:49 -1000

    remove md5 check
    Terrible for security but just want it work for now

--- modules/installer.sh
+++ modules/installer.sh
@@ -20,24 +20,16 @@ rm -rf /opt/pwnpad/web-interface/tmp/infusions
 
 mkdir -p /opt/pwnpad/web-interface/tmp/infusions
 wget -O /opt/pwnpad/web-interface/tmp/infusions/mk4-module-$name-$version.tar.gz "http://cloud.wifipineapple.com/index.php?downloads&downloadModule=$name&moduleVersion=$version"
-if [[ $(md5sum /opt/pwnpad/web-interface/tmp/infusions/mk4-module-$name-$version.tar.gz | head -c 33) == $md5 ]]
-	then
-			tar -xzf /opt/pwnpad/web-interface/tmp/infusions/mk4-module-$name-$version.tar.gz -C /opt/pwnpad/web-interface/tmp/infusions/
-				#get config stuff
-				config=$(cat /opt/pwnpad/web-interface/tmp/infusions/mk4-module-$name-$version/module.conf)
-				confName=$(echo "$config" | grep -i name | awk '{split($0,array,"=")} END{print array[2]}')
-				confVersion=$(echo "$config" | grep -i version | awk '{split($0,array,"=")} END{print array[2]}')
-				confAuthor=$(echo "$config" | grep -i author | awk '{split($0,array,"=")} END{print array[2]}')
-				confStartPage=$(echo "$config" | grep -i startPage | awk '{split($0,array,"=")} END{print array[2]}')
-				confSupportLink=$(echo "$config" | grep -i supportLink | sed 's/supportLink=//g')
-			mv /opt/pwnpad/web-interface/tmp/infusions/mk4-module-$name-$version/$confName /opt/pwnpad/web-interface/infusions/
-			rm -rf /opt/pwnpad/web-interface/tmp/infusions
-			echo "$confName|$confVersion|$dest|$confStartPage|$confSupportLink" >> /opt/pwnpad/web-interface/infusions/moduleList
-	else
-			sed -i 's/working/md5/g' installer.php
-			rm -rf /opt/pwnpad/web-interface/tmp/infusions
-			exit
-fi
-
+tar -xzf /opt/pwnpad/web-interface/tmp/infusions/mk4-module-$name-$version.tar.gz -C /opt/pwnpad/web-interface/tmp/infusions/
+#get config stuff
+	config=$(cat /opt/pwnpad/web-interface/tmp/infusions/mk4-module-$name-$version/module.conf)
+	confName=$(echo "$config" | grep -i name | awk '{split($0,array,"=")} END{print array[2]}')
+	confVersion=$(echo "$config" | grep -i version | awk '{split($0,array,"=")} END{print array[2]}')
+	confAuthor=$(echo "$config" | grep -i author | awk '{split($0,array,"=")} END{print array[2]}')
+	confStartPage=$(echo "$config" | grep -i startPage | awk '{split($0,array,"=")} END{print array[2]}')
+	confSupportLink=$(echo "$config" | grep -i supportLink | sed 's/supportLink=//g')
+mv /opt/pwnpad/web-interface/tmp/infusions/mk4-module-$name-$version/$confName /opt/pwnpad/web-interface/infusions/
+rm -rf /opt/pwnpad/web-interface/tmp/infusions
+echo "$confName|$confVersion|$dest|$confStartPage|$confSupportLink" >> /opt/pwnpad/web-interface/infusions/moduleList
 #Tell the installer that it is done
 sed -i 's/working/done/g' installer.php
