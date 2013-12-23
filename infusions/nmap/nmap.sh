a5ce2bbb7e434c076153f613ad80acfa6c562d10
Binky Bear <binkybear@local>
2013-12-22 20:58:50 -1000

    nmap fix

--- infusions/nmap/nmap.sh
+++ infusions/nmap/nmap.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-nmap -sV -T4 -O -F --version-light 172.16.42.112 -oN /opt/pwnpad/web-interface/infusions/nmap/scans/tmp 2>&1 && mv /pineapple/infusions/nmap/scans/tmp /pineapple/infusions/nmap/scans/scan_1361551911
+nmap -sV -T4 -O -F --version-light 172.16.42.112 -oN /opt/pwnpad/web-interface/infusions/nmap/scans/tmp 2>&1 && mv /opt/pwnpad/web-interface/infusions/nmap/scans/tmp /opt/pwnpad/web-interface/infusions/nmap/scans/scan_1361551911
